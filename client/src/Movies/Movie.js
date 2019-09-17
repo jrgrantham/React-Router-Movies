5b3925878b2cae385cfc4d60bc344fa4870f83f1
James Grantham <james.r.grantham@gmail.com>
2019-09-17 13:43:12 +0100

    Use match.params.id to return film

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
 
-  useEffect((props) => {
-    const id = props;
+  useEffect(() => {
+    // const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
         .then(response => {
           setMovie(response.data);
         })
@@ -18,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -22,7 +22,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
@@ -31,7 +33,6 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to='/movies/{id}'>
       <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
@@ -48,7 +49,6 @@ function MovieDetails({ movie }) {
           </div>
         ))}
       </div>
-    </Link>
   );
 }
 
